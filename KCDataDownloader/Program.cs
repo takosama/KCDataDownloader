e557489b4e238af636ae22959ba2b5aeffb0321f
tako <takosama.312@gmail.com>
2017-12-04 13:54:07 +0900

    艦船データのダウンロード実装

--- KCDataDownloader/Program.cs
+++ KCDataDownloader/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
 
 namespace KCDataDownloader
 {
@@ -10,6 +12,53 @@ namespace KCDataDownloader
     {
         static void Main(string[] args)
         {
+
+
+
+        }
+
+
+    }
+
+    static public class Downloader
+    {
+        readonly static string urlbase = "https://github.com/KC3Kai/kancolle-replay/tree/master/js/";
+        static public void DownloadShipData()
+        {
+            string urlTeel = "kcSHIPDATA.js";
+            string url = urlbase + urlTeel;
+            string path = "ShipData.js";
+            DownloadData(url, path);
+        }
+
+        static void DownloadData(string url, string path)
+        {
+            Console.WriteLine($"Start Download {url}");
+
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    WebClient wc = new WebClient();
+                    wc.DownloadFile(url, path);
+                    Console.WriteLine("Download 成功");
+                    return;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Download 失敗");
+                    Thread.Sleep(5);
+                    Console.WriteLine($"リトライします {i + 1}/5");
+
+                }
+            }
+            Console.WriteLine("Download 失敗");
+        }
+      
+        static public void DownloadItemDatas()
+        {
+
         }
     }
+
 }
